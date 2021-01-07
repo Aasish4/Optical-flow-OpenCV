9a3b42adbf2fc04de4f8304bb52ded7dcaf25580
Aasish KC <51996660+Aasish4@users.noreply.github.com>
2021-01-07 21:26:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -30,17 +30,17 @@ def main():
     args = parser.parse_args()
     video_path = args.video_path
     if args.algorithm == "lucaskanade":
-        lucas_kanade_method(video_path)
+        lucas_kanade(video_path)
     elif args.algorithm == "lucaskanade_dense":
         method = cv2.optflow.calcOpticalFlowSparseToDense
-        dense_optical_flow(method, video_path, to_gray=True)
+        dense_optical(method, video_path, to_gray=True)
     elif args.algorithm == "farneback":
         method = cv2.calcOpticalFlowFarneback
         params = [0.5, 3, 15, 3, 5, 1.2, 0]  # Farneback's algorithm parameters
-        dense_optical_flow(method, video_path, params, to_gray=True)
+        dense_optical(method, video_path, params, to_gray=True)
     elif args.algorithm == "rlof":
         method = cv2.optflow.calcOpticalFlowDenseRLOF
-        dense_optical_flow(method, video_path)
+        dense_optical(method, video_path)
 
 
 if __name__ == "__main__":
